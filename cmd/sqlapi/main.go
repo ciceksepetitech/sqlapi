996696a05868f54715ccf111fb1fa62dce07b7c8
Murat Alkan <murat.alkan@ciceksepeti.com>
2022-05-12 09:55:24 +0300

    Indent adjust

--- cmd/sqlapi/main.go
+++ cmd/sqlapi/main.go
@@ -5,13 +5,14 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/ciceksepetitech/sqlapi/internal/mongodb"
 	"github.com/ciceksepetitech/sqlapi/internal/mssql"
@@ -59,15 +60,15 @@ func query(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 		if v, ok := filter["_id"]; ok {
-       		if primitive.IsValidObjectID(v.(string)){
-            	id, err := primitive.ObjectIDFromHex(v.(string))
-        		if err != nil {
-        			panic(err)
-        		}    	
-				filter["_id"] = id  
-        	} else{
-            	filter["_id"] = v.(string)  
-        	}
+			if primitive.IsValidObjectID(v.(string)) {
+				id, err := primitive.ObjectIDFromHex(v.(string))
+				if err != nil {
+					panic(err)
+				}
+				filter["_id"] = id
+			} else {
+				filter["_id"] = v.(string)
+			}
 		}
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
