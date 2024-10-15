d7a59904e2f410e0e18e628aa8f64c483e4b516b
csalialtog <ali.altog@gmail.com>
2024-10-15 16:02:29 +0300

    multi platform added

---
+++ build-image-multi-platform.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+NAME=$1
+DOCKERFILE=$2
+TAG=$3
+PLATFORM=${4:-linux/amd64,linux/arm64}  # Default platform is linux/amd64 and linux/arm64 if not specified
+
+if [ $TAG ]; then
+   echo "Building dockerfile $DOCKERFILE for $NAME:$TAG on platforms $PLATFORM"
+   docker buildx build --rm=false --file ./images/$DOCKERFILE --platform $PLATFORM -t $NAME:$TAG ./images/. --push
+else
+   echo "Building dockerfile $DOCKERFILE for $NAME on platforms $PLATFORM"
+   docker buildx build --rm=false --file ./images/$DOCKERFILE --platform $PLATFORM -t $NAME ./images/. --push
+   docker tag $NAME $NAME:latest
+   docker tag $NAME $NAME:$BUILDPREFIX$GITHUB_RUN_NUMBER
+fi
